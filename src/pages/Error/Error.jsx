3e173780dd876301cffcf48bae20eae21d09f171
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-02-10 23:26:48 +0100

    Error page, css

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,34 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+const ErrorContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const ErrorTitle = styled.h1`
+    font-size: 288px;
+    font-weight: 700;
+    margin-bottom: 3vh;
+`
+
+const ErrorMessage = styled.p`
+    font-size: 36px;
+    font-weight: 500;
+    margin-bottom: 10vh;
+`
+
+const ErrorLink = styled(Link)`
+    font-size: 18px;
+`
 
 export const Error = () => {
     return (
-        <div>
-            <h1>404</h1>
-        </div>
+        <ErrorContainer>
+            <ErrorTitle>404</ErrorTitle>
+            <ErrorMessage>Oups! La page que vous demandez n'existe pas.</ErrorMessage>
+            <ErrorLink to='/' >Retourner sur la page d’accueil</ErrorLink>
+        </ErrorContainer>
     );
 };  
--- src/style/GlobalStyles.js
+++ src/style/GlobalStyles.js
@@ -4,7 +4,12 @@ import colors from './colors'
 export const GlobalStyles = createGlobalStyle`
     body {
         font-family: 'Montserrat', sans-serif;
+        color: ${colors.primary};
         margin: 0;
         padding: 40px 100px;
+    }
+
+    a:visited {
+        color: ${colors.primary};
     }
 `
