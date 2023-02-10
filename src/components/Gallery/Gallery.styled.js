b074fb06349a4ab6bdd71677f81254cf58256a00
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-02-10 22:24:26 +0100

    Gallery Component

---
+++ src/components/Gallery/Gallery.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import colors from "../../style/colors";
+
+export const Gallery = styled.div`
+    width: 100%;
+    height: 1000px;
+    background: ${colors.backgroundLight};
+    border-radius: 25px;
+`
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import image from '../../images/home-bg.jpg'
-import { Banner } from '../../components/Banner/Banner'
+import { Banner } from '../../components/Banner/Banner.styled'
+import { Gallery } from '../../components/Gallery/Gallery.styled';
 
 export const Home = () => {
   return (
     <div className="Home">
       <Banner src={image} alt="Checz vous, partout et ailleurs"/>
+      <Gallery />
     </div>
   );
 };
