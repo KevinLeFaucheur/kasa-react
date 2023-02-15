6d413880ddb20e41d4304df0a48a96659b669da4
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-02-15 21:35:01 +0100

    About Banner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Nav } from '../Nav/Nav.styled' 
 import { StyledNavLink } from '../NavLink/NavLink.styled'
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { StyledHeader } from './Header.styled'
 import { KasaLogo } from '../../style/logo'
 import colors from '../../style/colors';
@@ -13,6 +13,8 @@ export const Header = () => {
             <Nav>
                 <StyledNavLink to='/'>Accueil</StyledNavLink>
                 <StyledNavLink to='/about'>A Propos</StyledNavLink>
+                {/* <StyledNavLink style={({isActive}) => { return isActive ? { color: 'blue' } : {}}} to='/lodging'>Test</StyledNavLink> */}
+                {/* <NavLink style={({isActive}) => { return isActive ? { color: 'blue' } : {}}} to='/lodging'>Test</NavLink> */}
             </Nav>
         </StyledHeader>
     )
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,20 @@
+import Collapse from '../../components/Collapse/Collapse'
+import styled from 'styled-components';
+import image from '../../images/kalen-emsley-Bkci_8qcdvQ-unsplash.jpg'
+
+const Banner = styled.div`
+  height: 223px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(${image});
+  border-radius: 25px;
+  background-size: cover;
+  background-position: center;
+`
+
 export const About = () => {
   return (
     <div className="About">
-      <h1>A Propos</h1>
+      <Banner />
+      {/* <Collapse /> */}
     </div>
   );
 };
