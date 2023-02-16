7a57254a6ef42ffef19c1947d4dc8def59bdc9fa
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-02-16 23:57:28 +0100

    Rating functionality

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,15 +1,21 @@
+import styled from "styled-components";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const RatingWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+
 export const Rating = ({ rating }) => {
     return (
-        <div>
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStar} />
-            <FontAwesomeIcon icon={faStarRegular} />
-            <FontAwesomeIcon icon={faStarRegular} />
-        </div>
+        <RatingWrapper>
+            {[...Array(5)].map((_, index) => {
+                console.log(rating)
+                return rating > index ? <FontAwesomeIcon key={index} icon={faStar}/> 
+                                      : <FontAwesomeIcon key={index} icon={faStarRegular} />
+            })}
+        </RatingWrapper>
     )
 }
