ccff26389aaf9b87575d52553a01c3971e90012e
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-03-01 21:03:20 +0100

    Typo

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { GlobalStyles } from './style/GlobalStyles';
 
 import { Home } from './pages/Home';
 import { Lodging } from './pages/Lodging';
-import { About } from './pages/About'
-import { Error } from './pages/Error'
-import { Header } from './components/Header'
+import { About } from './pages/About';
+import { Error } from './pages/Error';
+import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
